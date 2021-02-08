502f12b9d996980c63abc2530e70e6524da5375e
Liubomyr <lyubomyrklymyuk2001@gmail.com>
2021-02-08 15:10:27 +0200

    second

--- main.js
+++ main.js
@@ -3,3 +3,36 @@
 const category = 'toys';
 
  console.log(`https://someurl.com/${category}/5`);
+
+ console.log(`arr` + `-object`); // arr-object
+ console.log( 4 + ' - object'); // 4 - object
+ console.log( 4 + +'-object'); //NaN
+ console.log( 4 + +'5'); // 9 
+git
+
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+console.log(5%2); // 1 
+console.log(2*4 == "8"); // true
+console.log(2*4 === "8"); //false
+
+
+const isChekced = true,
+      isClose = true;
+  
+console.log(isChekced && isClose); // true if isClose == false - false
+console.log(isChekced || isClose); //  is true even isClose == false
+
+const forNewCommit = 1;
