03664ba2ef80a5cf3cd4c153985648bda0f60a43
Liubomyr <lyubomyrklymyuk2001@gmail.com>
2021-02-27 21:10:46 +0200

    i`ve passed theme of function and it`s practice #3`

--- main.js
+++ main.js
@@ -1,20 +1,61 @@
 'use strict';
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+ 
+let numberOfFilms;
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors:{},
-    genres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors:{},
+  genres: [],
+  privat: false
 };
 
-const a = prompt('Один из последних просмотреных фильмов',''),
-      b = +prompt('Насколько оцение его',''),
-      c = prompt('Один из последних просмотреных фильмов',''),
-      d = +prompt('Насколько оцение его','');
+start();
+detectedPersonalLevel();
+rememberMyFilms();
+showMeDB(personalMovieDB.privat);
+writeYourGenres();
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+function writeYourGenres(){
+  for(let i = 1; i<=3; i++){
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр фильма ${i}`);
+  }
+}
 
-console.log(personalMovieDB);
+function showMeDB(hidden){
+  if(!hidden){
+    console.log(personalMovieDB);
+  }
+}
+
+function start(){
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+  }
+}
+
+function detectedPersonalLevel(){
+  let statusOfUser = (numberOfFilms < 10 && numberOfFilms> 0) ? 'Достаточно мало фильмов': 
+  (numberOfFilms>=10 && numberOfFilms<=30) ? 'Вы обычный зритель' :
+  (numberOfFilms>30) ? 'Ого вы киноман' : 
+   'Произошла ошибка';
+alert(statusOfUser);
+}
+
+function rememberMyFilms(){
+
+  for (let i=0; i<2; i++){
+    const a = prompt('Один из последних просмотреных фильмов', ''),
+          b = +prompt('Насeколько оцение его', '');
+
+          if (a != null &&  b !== null && a !='' && b != '' && a.length < 50){
+            personalMovieDB.movies[a]=b;
+            console.log('done');
+          } else {
+               console.log('error');
+               i--; 
+          }
+  }
+}
